0f157356d9e2fba7f5a4cdea2d0d0c2068285424
LuizCarlosIFBA <sacramento.tecnico@gmail.com>
2021-06-01 12:01:12 -0300

    ok

--- Trabalho3/index.py
+++ Trabalho3/index.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
 
+#busca por indice numérico
 def binary_search(array, item, begin=0, end=None):
+    if item<4 or item>6:
+            print("Não encontrado")
+            sys.exit()  
     if end is None:
         end = len(array)-1
     if begin <= end:
@@ -14,13 +18,34 @@ def binary_search(array, item, begin=0, end=None):
             return binary_search(array, item, m+1, end)     
     return None       
 
+#busca por indice alfabético
+
+def binary_search_city(name):
+    item = 0
+    if name =="Cerejeiras":
+            item = 4
+    else: 
+        if name == "Colorado do Oeste":
+                item = 5      
+        else: 
+            if name == "Corumbiara":
+                    item = 6                
+            else:
+                 print("Não encontrado")
+                 sys.exit()      
+    return item       
+
+
 vet = [4,5,6];#vetor auxiliar
 
 mydata = pd.read_csv("Municipios.csv")
 mydata_array = np.array(mydata)
-print(mydata_array[binary_search(vet, 4)])#busca por chave numérica
 
-
-
+print("\nBusca por chave numérica:\n")
+print(mydata_array[binary_search(vet, 5)])#busca por chave numérica
+print("\nBusca por chave alfabética:\n")
+print(mydata_array[binary_search(vet, binary_search_city("Cerejeiras"))])#busca por chave alfabética
+print("\nBusca por letra:\n")
+print(mydata.query('"S" <= Municipio <= "S~"'))#busca por letra
 
 
